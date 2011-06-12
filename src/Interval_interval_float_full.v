f1704b6ccbf5ab0435d7cb85be0dc68ab80d1dcc
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2011-06-12 10:00:22 +0200

    Proved sin_correct.

--- src/Interval_interval_float_full.v
+++ src/Interval_interval_float_full.v
@@ -17,6 +17,25 @@ Definition Fle x y :=
   | Xgt | Xund => false
   end.
 
+Lemma Fle_correct :
+  forall x y,
+  Fle x y = true ->
+  match I.convert_bound x, I.convert_bound y with
+  | Xreal xr, Xreal yr => (xr <= yr)%R
+  | _, _ => False
+  end.
+Proof.
+intros x y.
+unfold Fle.
+rewrite F.cmp_correct, Interval_generic_proof.Fcmp_correct.
+I.xreal_tac x.
+easy.
+I.xreal_tac y.
+easy.
+simpl.
+now case Fcore_Raux.Rcompare_spec ; auto with real.
+Qed.
+
 (* useful only for |xi| <= pi *)
 Definition cos prec xi :=
   match I.abs xi with
@@ -34,7 +53,7 @@ Axiom cos_correct : forall prec, I.extension Xcos (cos prec).
 Definition sin prec xi :=
   match xi with
   | Ibnd xl xu =>
-    let pi2 := F.scale (I.lower (T.pi4 prec)) (F.ZtoS 1%Z) in
+    let pi2 := F.scale2 (I.lower (T.pi4 prec)) (F.ZtoS 1%Z) in
     match Fle (F.neg pi2) xl, Fle xu pi2 with
     | true, true =>
       I.bnd (I.lower (T.sin_fast prec xl)) (I.upper (T.sin_fast prec xu))
@@ -43,13 +62,108 @@ Definition sin prec xi :=
   | Inan => Inan
   end.
 
-Axiom sin_correct : forall prec, I.extension Xsin (sin prec).
+Theorem sin_correct :
+  forall prec, I.extension Xsin (sin prec).
+Proof.
+intros prec [|xl xu] [|x] ; try easy.
+intros (Hxl, Hxu).
+simpl.
+set (pi2 := F.scale2 (I.lower (T.pi4 prec)) (F.ZtoS 1)).
+case_eq (Fle (F.neg pi2) xl) ; intros Hpl.
+generalize (Fle_correct _ _ Hpl).
+I.xreal_tac xl. now case (I.convert_bound (F.neg pi2)).
+clear Hpl. intros Hpl.
+case_eq (Fle xu pi2) ; intros Hpu.
+generalize (Fle_correct _ _ Hpu).
+I.xreal_tac xu. easy.
+I.xreal_tac pi2. easy.
+clear Hpu. intros Hpu.
+revert Hpl.
+unfold I.convert_bound.
+rewrite F.neg_correct, Interval_generic_proof.Fneg_correct.
+fold (I.convert_bound pi2).
+rewrite X1.
+simpl.
+intros Hpl.
+generalize (F.scale2_correct (I.lower (T.pi4 prec)) 1 (refl_equal _)).
+rewrite Interval_generic_proof.Fscale2_correct with (1 := F.even_radix_correct).
+intros X2.
+change (I.convert_bound pi2 = Xmul (I.convert_bound (I.lower (T.pi4 prec))) (Xreal 2)) in X2.
+rewrite X1 in X2. clear X1.
+revert X2.
+generalize (T.pi4_correct prec).
+case (T.pi4 prec).
+simpl.
+now unfold I.convert_bound ; rewrite F.nan_correct.
+simpl.
+intros p.
+change (T.I.convert_bound p) with (I.convert_bound p).
+I.xreal_tac p. easy.
+intros _ (Hp,_) H.
+injection H.
+clear H X1. intros H.
+(* *)
+assert (Hpl': (-(PI/2) <= r)%R).
+apply Rle_trans with (2 := Hpl).
+apply Ropp_le_contravar.
+rewrite H.
+replace (PI / 2)%R with (PI / 4 * 2)%R by field.
+apply Rmult_le_compat_r with (2 := Hp).
+now apply (Fcore_Raux.Z2R_le 0 2).
+assert (Hpu': (r0 <= PI/2)%R).
+apply Rle_trans with (1 := Hpu).
+rewrite H.
+replace (PI / 2)%R with (PI / 4 * 2)%R by field.
+apply Rmult_le_compat_r with (2 := Hp).
+now apply (Fcore_Raux.Z2R_le 0 2).
+split.
+(* *)
+generalize (T.sin_fast_correct prec xl).
+destruct (T.sin_fast prec xl) as [|yl yu].
+unfold I.convert_bound. simpl.
+now rewrite F.nan_correct.
+fold (I.convert_bound xl). rewrite X.
+simpl.
+change (T.I.convert_bound yl) with (I.convert_bound yl).
+I.xreal_tac yl. easy.
+change (T.I.convert_bound yu) with (I.convert_bound yu).
+intros (Hy,_).
+apply Rle_trans with (1 := Hy).
+assert (H' := Rle_trans _ _ _ Hxu Hpu').
+apply sin_incr_1 ; try easy.
+now apply Rle_trans with x.
+now apply Rle_trans with r.
+(* *)
+generalize (T.sin_fast_correct prec xu).
+destruct (T.sin_fast prec xu) as [|yl yu].
+unfold I.convert_bound. simpl.
+now rewrite F.nan_correct.
+fold (I.convert_bound xu). rewrite X0.
+simpl.
+change (T.I.convert_bound yu) with (I.convert_bound yu).
+I.xreal_tac yu. easy.
+intros (_, Hy).
+apply Rle_trans with (2 := Hy).
+assert (H' := Rle_trans _ _ _ Hpl' Hxl).
+apply sin_incr_1 ; try easy.
+now apply Rle_trans with r0.
+now apply Rle_trans with x.
+(* *)
+simpl.
+unfold I.convert_bound.
+rewrite 2!F.fromZ_correct.
+apply SIN_bound.
+simpl.
+unfold I.convert_bound.
+rewrite 2!F.fromZ_correct.
+apply SIN_bound.
+Qed.
 
 (* meaningful only for |xi| <= pi/2 *)
 Definition tan prec xi :=
   match xi with
   | Ibnd xl xu =>
-    let pi2 := F.scale (I.lower (T.pi4 prec)) (F.ZtoS 1%Z) in
+    let pi2 := F.scale2 (I.lower (T.pi4 prec)) (F.ZtoS 1%Z) in
     match Fle (F.neg pi2) xl, Fle xu pi2 with
     | true, true =>
       I.bnd (I.lower (T.tan_fast prec xl)) (I.upper (T.tan_fast prec xu))
@@ -65,9 +179,9 @@ Definition atan prec xi :=
   | Ibnd xl xu =>
     Ibnd
      (if F.real xl then I.lower (T.atan_fast prec xl)
-      else F.neg (F.scale (I.upper (T.pi4 prec)) (F.ZtoS 1%Z)))
+      else F.neg (F.scale2 (I.upper (T.pi4 prec)) (F.ZtoS 1%Z)))
      (if F.real xu then I.upper (T.atan_fast prec xu)
-      else F.scale (I.upper (T.pi4 prec)) (F.ZtoS 1%Z))
+      else F.scale2 (I.upper (T.pi4 prec)) (F.ZtoS 1%Z))
   | Inan => Inan
   end.
 
