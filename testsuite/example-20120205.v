f14f6ebd75858aef7b9037f92c51b063c8724e77
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2012-07-06 19:21:39 +0200

    New release.

--- testsuite/example-20120205.v
+++ testsuite/example-20120205.v
@@ -15,3 +15,8 @@ Goal forall x, (2 <= x)%R -> (x < x * x)%R.
 intros.
 interval with (i_bisect_diff x).
 Qed.
+
+Goal forall x, (-1 <= x)%R -> (x < 1 + powerRZ x 3)%R.
+intros.
+interval with (i_bisect_diff x).
+Qed.
