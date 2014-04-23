852bf22fa398383735afa51d6c859457f660554a
Erik Martin-Dorel <erik.martin-dorel@ens-lyon.org>
2014-04-23 14:50:25 +0200

    Do some cleanup in testsuite/example-20140221.v.
    - No need for Interval_missing anymore
    - Don't use scope suffixes, but Open Scope R_scope
    - Drop the timings that were obtained using Coq 8.4pl2

--- testsuite/example-20140221.v
+++ testsuite/example-20140221.v
@@ -1,6 +1,7 @@
 Require Import Reals.
 Require Import Interval_tactic.
-Require Import Interval_missing. (* for Z2R_IZR, pow_powerRZ *)
+
+Local Open Scope R_scope.
 
 (*
 Example taken from:
@@ -9,39 +10,35 @@ In TPHOLs, pages 137-152, 1997.
 *)
 
 Notation claim :=
-  (forall x : R,
+  ( forall x : R,
     (-10831/1000000 <= x /\ x <= 10831/1000000) ->
     Rabs ((exp x - 1) - (x + (8388676/2^24) * x^2 + (11184876/2^26) * x^3))
-    <= ((23/27) / (2^33)))%R
+    <= ((23/27) / (2^33)) )
   (only parsing).
 
 Goal claim.
 intros x H.
-interval_intro (10831/1000000)%R upper.
-interval_intro (-10831/1000000)%R lower.
+interval_intro (10831/1000000) upper.
+interval_intro (-10831/1000000) lower.
 generalize (conj (Rle_trans _ _ _ H1 (proj1 H)) (Rle_trans _ _ _ (proj2 H) H0)).
 clear; intros H.
 
-Time interval_intro (exp x - 1)%R with (i_bisect_taylor x 2).
+Time interval_intro (exp x - 1) with (i_bisect_taylor x 2).
 clear H0.
 
 Time interval with (i_bisect_taylor x 3, i_prec 50, i_depth 7).
-(* No more subgoals. *)
-(* Finished transaction in 0. secs (0.593962u,0.003918s) *)
 Qed.
 
 (*
 Goal claim.
 intros x H.
-interval_intro (23 / 27 / 2 ^ 33)%R lower.
+interval_intro (23 / 27 / 2 ^ 33) lower.
 apply Rle_trans with (2 := H0); clear H0.
-interval_intro (10831/1000000)%R upper.
-interval_intro (-10831/1000000)%R lower.
+interval_intro (10831/1000000) upper.
+interval_intro (-10831/1000000) lower.
 generalize (conj (Rle_trans _ _ _ H1 (proj1 H)) (Rle_trans _ _ _ (proj2 H) H0)).
 clear; intros H.
 
 Time interval with (i_bisect_diff x, i_prec 50, i_depth 16).
-(* No more subgoals. *)
-(* Finished transaction in 33. secs (32.86976u,0.007909s) *)
 Qed.
 *)
