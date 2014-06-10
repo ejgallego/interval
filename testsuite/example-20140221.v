52b9398c6fbe6a0110b08a4e83552dca9d1cc778
Erik Martin-Dorel <erik.martin-dorel@ens-lyon.org>
2014-06-10 20:55:34 +0200

    Do some cleanup in example-20140221.v
    Simplify the bookkeeping steps before calling interval, in particular:
    [generalize (conj … …); intros H] => [pose proof (conj … …) as H].

--- testsuite/example-20140221.v
+++ testsuite/example-20140221.v
@@ -17,29 +17,23 @@ Notation claim :=
   (only parsing).
 
 Goal claim.
-intros x H.
-interval_intro (10831/1000000) upper.
-interval_intro (-10831/1000000) lower.
-generalize (conj (Rle_trans _ _ _ H1 (proj1 H)) (Rle_trans _ _ _ (proj2 H) H0)).
-clear; intros H.
-
-Time interval_intro (exp x - 1) with (i_bisect_taylor x 2).
-clear H0.
-
-Time interval with (i_bisect_taylor x 3, i_prec 50, i_depth 5).
-  (* with a basic Horner's scheme, needs i_depth 7 *)
-Qed.
-
-(*
-Goal claim.
-intros x H.
-interval_intro (23 / 27 / 2 ^ 33) lower.
+intros x [_x x_].
+interval_intro (-10831/1000000) lower as H1.
+interval_intro (10831/1000000) upper as H2.
+apply Rle_trans with (1 := H1) in _x; clear H1.
+apply Rle_trans with (2 := H2) in x_; clear H2.
+(* Erik: the following line should be optional *)
+pose proof (conj _x x_) as Hx; clear _x x_.
+(* Erik: the following 2 lines are required only for i_bisect_diff *)
+interval_intro (23 / 27 / 2 ^ 33) lower as H0.
 apply Rle_trans with (2 := H0); clear H0.
-interval_intro (10831/1000000) upper.
-interval_intro (-10831/1000000) lower.
-generalize (conj (Rle_trans _ _ _ H1 (proj1 H)) (Rle_trans _ _ _ (proj2 H) H0)).
-clear; intros H.
 
+Time interval with (i_bisect_taylor x 3, i_prec 50).
+(* Finished transaction in 0. secs (0.382009u,0.002368s) *)
+
+(* Undo.
 Time interval with (i_bisect_diff x, i_prec 50, i_depth 16).
-Qed.
+(* Finished transaction in 49. secs (48.178082u,0.047943s) *)
 *)
+
+Qed.
