648cca7df98fb618423667c3dc00e3e2845faaf3
Erik Martin-Dorel <erik.martin-dorel@ens-lyon.org>
2015-07-14 17:08:45 +0200

    Simplify the proof script of example-20140221.v.

--- testsuite/example-20140221.v
+++ testsuite/example-20140221.v
@@ -9,15 +9,15 @@ John Harrison, Verifying the Accuracy of Polynomial Approximations in HOL.
 In TPHOLs, pages 137-152, 1997.
 *)
 
-Notation claim :=
-  ( forall x : R,
+Goal
+  forall x : R,
     (-10831/1000000 <= x /\ x <= 10831/1000000) ->
     Rabs ((exp x - 1) - (x + (8388676/2^24) * x^2 + (11184876/2^26) * x^3))
-    <= ((23/27) / (2^33)) )
-  (only parsing).
-
-Goal claim.
-intros x [_x x_].
+    <= (23/27) / (2^33).
+Proof.
+intros x H.
+(*
+destruct H as [_x x_].
 interval_intro (-10831/1000000) lower as H1.
 interval_intro (10831/1000000) upper as H2.
 apply Rle_trans with (1 := H1) in _x; clear H1.
@@ -27,13 +27,9 @@ pose proof (conj _x x_) as Hx; clear _x x_.
 (* Erik: the following 2 lines are required only for i_bisect_diff *)
 interval_intro (23 / 27 / 2 ^ 33) lower as H0.
 apply Rle_trans with (2 := H0); clear H0.
-
-Time interval with (i_bisect_taylor x 3, i_prec 50).
-(* Finished transaction in 0. secs (0.382009u,0.002368s) *)
-
-(* Undo.
-Time interval with (i_bisect_diff x, i_prec 50, i_depth 16).
-(* Finished transaction in 49. secs (48.178082u,0.047943s) *)
+Time interval with (i_bisect_diff x, i_prec 50, i_depth 16). (* 31 s *)
 *)
-
+Time interval with (i_bisect_taylor x 3, i_prec 50). (* 0.48 s *)
 Qed.
+
+(* The timings above were obtained using Coq 8.4pl6 *)
