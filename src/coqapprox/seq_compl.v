8983c82c3102f89b94aaff7d6b43bcecd0ced3c5
Erik Martin-Dorel <erik.martin-dorel@ens-lyon.org>
2015-12-13 21:57:14 +0100

    seq_compl: Prove fold_correct and mkseq_correct.
    Refactor previous lemmas as well, by locally defining a predicate:
    Let RelP sv st := forall k : nat, Rel (nth dv sv k) (nth dt st k).

--- src/coqapprox/seq_compl.v
+++ src/coqapprox/seq_compl.v
@@ -24,6 +24,8 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
+(** Missing results about natural numbers *)
+
 Section NatCompl.
 
 Lemma nat_ind_gen (P : nat -> Prop) :
@@ -65,6 +67,29 @@ Qed.
 
 End NatCompl.
 
+(** Missing results about lists (aka sequences) *)
+
+Section Head_Last.
+Variables (T : Type) (d : T).
+
+Lemma head_cons : forall s, s <> [::] -> s = head d s :: behead s.
+Proof. by case. Qed.
+
+Definition hb s := head d (behead s).
+
+Lemma nth1 : forall s, nth d s 1 = hb s.
+Proof. by move=> s; rewrite /hb -nth0 nth_behead. Qed.
+
+Lemma last_rev : forall s, last d (rev s) = head d s.
+Proof. by elim=> [//|s IHs]; rewrite rev_cons last_rcons. Qed.
+
+Definition rmlast (l : seq T) := (belast (head d l) (behead l)).
+End Head_Last.
+
+Lemma rmlast_cons T (d e f : T) (s : seq T) :
+  s <> [::] -> rmlast e (f :: s) = f :: rmlast d s.
+Proof. by case: s. Qed.
+
 Section Take.
 Variable (T : Type).
 
@@ -82,28 +107,6 @@ Proof.
 case: (leqP (size s) n); last exact: nth_map.
 by move=> ?; rewrite nth_default // size_map.
 Qed.
-
-Section map_proof.
-Variables (V T : Type) (Rel : V -> T -> Prop).
-Variables (vop : V -> V) (top : T -> T).
-Variables (dv : V) (dt : T).
-Hypothesis H0 : Rel dv dt.
-Hypothesis H0t : forall v : V, Rel v dt -> Rel (vop v) dt.
-Hypothesis H0v : forall t : T, Rel dv t -> Rel dv (top t).
-Lemma map_correct :
-  forall sv st,
-    (forall v t, Rel v t -> Rel (vop v) (top t)) ->
-    (forall k : nat, Rel (nth dv sv k) (nth dt st k)) ->
-    forall k : nat, Rel (nth dv (map vop sv) k) (nth dt (map top st) k).
-Proof.
-move=> sv st Hop Hnth k; move/(_ k) in Hnth.
-rewrite !nth_map_dflt.
-do 2![case:ifP]=> A B //; rewrite ?(nth_default _ A) ?(nth_default _ B) in Hnth.
-- exact: H0v.
-- exact: H0t.
-- exact: Hop.
-Qed.
-End map_proof.
 
 Section Map2.
 Variable C : Type.
@@ -141,12 +144,63 @@ elim: s1 s2 n => [|x1 s1 IHs1] s2 n; case: s2 => [|x2 s2] //=.
 Qed.
 End Map2.
 
+Lemma nth_mkseq_dflt (C : Type) (x0 : C) (f : nat -> C) (n i : nat) :
+  nth x0 (mkseq f n) i = if n <= i then x0 else f i.
+Proof.
+case: (leqP n i); last exact: nth_mkseq.
+by move=> ?; rewrite nth_default // size_mkseq.
+Qed.
+
+Section Fold.
+(* Erik: is this still used in the library ? *)
+Variables (A B : Type) (f : A -> B).
+
+Lemma foldr_cons (r : seq A) (s : seq B) :
+  foldr (fun x acc => f x :: acc) s r = map f r ++ s.
+Proof. by elim: r => [//|x r' IHr]; rewrite /= -IHr. Qed.
+
+Corollary foldr_cons0 (r : seq A) :
+  foldr (fun x acc => f x :: acc) [::] r = map f r.
+Proof. by rewrite foldr_cons cats0. Qed.
+
+Lemma foldl_cons (r : seq A) (s : seq B) :
+  foldl (fun acc x => f x :: acc) s r = (catrev (map f r) s).
+Proof. by elim: r s => [//|a r' IHr] s; rewrite /= -IHr. Qed.
+
+Corollary foldl_cons0 (r : seq A) :
+  foldl (fun acc x => f x :: acc) [::] r = rev (map f r).
+Proof. by rewrite foldl_cons. Qed.
+End Fold.
+
+(** Generic results to be instantiated for polynomials' opp, add, sub, mul... *)
+
+Section map_proof.
+Variables (V T : Type) (Rel : V -> T -> Prop).
+Variables (dv : V) (dt : T).
+Let RelP sv st := forall k : nat, Rel (nth dv sv k) (nth dt st k).
+Variables (vop : V -> V) (top : T -> T).
+Hypothesis H0 : Rel dv dt.
+Hypothesis H0t : forall v : V, Rel v dt -> Rel (vop v) dt.
+Hypothesis H0v : forall t : T, Rel dv t -> Rel dv (top t).
+Hypothesis Hop : forall v t, Rel v t -> Rel (vop v) (top t).
+Lemma map_correct :
+  forall sv st, RelP sv st -> RelP (map vop sv) (map top st).
+Proof.
+move=> sv st Hnth k; move/(_ k) in Hnth.
+rewrite !nth_map_dflt.
+do 2![case:ifP]=> A B //; rewrite ?(nth_default _ A) ?(nth_default _ B) in Hnth.
+- exact: H0v.
+- exact: H0t.
+- exact: Hop.
+Qed.
+End map_proof.
+
 Section map2_proof.
 Variables (V T : Type) (Rel : V -> T -> Prop).
-Variables (vop : V -> V -> V) (top : T -> T -> T).
 Variables (dv : V) (dt : T).
+Let RelP sv st := forall k : nat, Rel (nth dv sv k) (nth dt st k).
+Variables (vop : V -> V -> V) (top : T -> T -> T).
 Hypothesis H0 : Rel dv dt.
-
 Hypothesis H0eq : forall v, Rel v dt -> v = dv.
 
 Hypothesis H0t1 : forall (v1 v2 : V) (t1 : T), Rel v1 t1 ->
@@ -165,10 +219,10 @@ Hypothesis Hop : (forall v1 v2 t1 t2, Rel v1 t1 -> Rel v2 t2 -> Rel (vop v1 v2) 
 
 Lemma map2_correct :
   forall sv1 sv2 st1 st2,
-  (forall k : nat, Rel (nth dv sv1 k) (nth dt st1 k)) ->
-  (forall k : nat, Rel (nth dv sv2 k) (nth dt st2 k)) ->
-  forall k : nat, Rel (nth dv (map2 vop sv1 sv2) k) (nth dt (map2 top st1 st2) k).
-Proof using H0 H0t1 H0t2 H0v1 H0v2 Hop H0eq.
+    RelP sv1 st1 ->
+    RelP sv2 st2 ->
+    RelP (map2 vop sv1 sv2) (map2 top st1 st2).
+Proof using RelP H0 H0t1 H0t2 H0v1 H0v2 Hop H0eq.
 move=> sv1 sv2 st1 st2 H1 H2 k; move/(_ k) in H1; move/(_ k) in H2.
 rewrite !nth_map2_dflt.
 do 4![case:ifP]=> A B C D; rewrite
@@ -179,65 +233,40 @@ do 4![case:ifP]=> A B C D; rewrite
 Qed.
 End map2_proof.
 
+Section fold_proof.
+Variables (V T : Type).
+Variable Rel : V -> T -> Prop.
+Variables (dv : V) (dt : T).
+Hypothesis H0 : Rel dv dt.
 (*
-Lemma nth_map2 s1 s2 (k : nat) da (x y : V) (g : V -> V -> V) :
-  A (nth da s1 k) x ->
-  A (nth da s2 k) y ->
-  A (nth da (map2 s1 s2) k) (g x y).
-Proof.
-elim: s1 s2 k => [|x1 s1 IH1] s2 k Hx Hy.
-  rewrite nth_default // in Hx *.
-  admit.
-  (* by rewrite (size0nil) !nth_nil.*)
-case: s2 Hy =>[//|x2 s2]  Hy; rewrite nth_default //=.
-  rewrite nth_default //in Hy.
-  admit.
-  admit.
-admit.
-(* case: k IH1 =>[//|k]; exact. *)
-Qed.
-
-End Map2.
+Let RelP sv st := forall k : nat, Rel (nth dv sv k) (nth dt st k).
+Variables (vadd : V -> V -> V) (tadd : T -> T -> T).
+Variables (vmul : V -> V -> V) (tmul : T -> T -> T).
 *)
+Lemma fold_correct A fv ft (s : seq A) :
+  (forall a v t, Rel v t -> Rel (fv a v) (ft a t)) ->
+  Rel (foldr fv dv s) (foldr ft dt s).
+Proof.
+move=> Hf.
+elim: s => [ | a s IHs]; first exact: H0.
+apply: Hf.
+exact: IHs.
+Qed.
+End fold_proof.
 
-Section Head_Last.
-Variables (T : Type) (d : T).
-
-Lemma head_cons : forall s, s <> [::] -> s = head d s :: behead s.
-Proof. by case. Qed.
-
-Definition hb s := head d (behead s).
-
-Lemma nth1 : forall s, nth d s 1 = hb s.
-Proof. by move=> s; rewrite /hb -nth0 nth_behead. Qed.
-
-Lemma last_rev : forall s, last d (rev s) = head d s.
-Proof. by elim=> [//|s IHs]; rewrite rev_cons last_rcons. Qed.
-
-Definition rmlast (l : seq T) := (belast (head d l) (behead l)).
-End Head_Last.
-
-Lemma rmlast_cons T (d e f : T) (s : seq T) :
-  s <> [::] -> rmlast e (f :: s) = f :: rmlast d s.
-Proof. by case: s. Qed.
-
-Section Fold.
-Variables (A B : Type) (f : A -> B).
-
-Lemma foldr_cons (r : seq A) (s : seq B) :
-  foldr (fun x acc => f x :: acc) s r = map f r ++ s.
-Proof. by elim: r => [//|x r' IHr]; rewrite /= -IHr. Qed.
-
-Corollary foldr_cons0 (r : seq A) :
-  foldr (fun x acc => f x :: acc) [::] r = map f r.
-Proof. by rewrite foldr_cons cats0. Qed.
-
-Lemma foldl_cons (r : seq A) (s : seq B) :
-  foldl (fun acc x => f x :: acc) s r = (catrev (map f r) s).
-Proof. by elim: r s => [//|a r' IHr] s; rewrite /= -IHr. Qed.
-
-Corollary foldl_cons0 (r : seq A) :
-  foldl (fun acc x => f x :: acc) [::] r = rev (map f r).
-Proof. by rewrite foldl_cons. Qed.
-
-End Fold.
+Section mkseq_proof.
+Variables (V T : Type).
+Variable Rel : V -> T -> Prop.
+Variables (dv : V) (dt : T).
+Let RelP sv st := forall k : nat, Rel (nth dv sv k) (nth dt st k).
+Hypothesis H0 : Rel dv dt.
+Lemma mkseq_correct fv ft n :
+  (forall k : nat, Rel (fv k) (ft k)) ->
+  RelP (mkseq fv n) (mkseq ft n).
+Proof.
+move=> Hk k; rewrite /RelP !nth_mkseq_dflt.
+case: ifP=> _.
+- exact: H0.
+- exact: Hk.
+Qed.
+End mkseq_proof.
