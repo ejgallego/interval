467933a1b561fa04eba488e8fd3f853c003d5c59
Erik Martin-Dorel <erik.martin-dorel@ens-lyon.org>
2015-12-26 14:44:28 +0100

    reals_tac.v: Fix add_eq and elim_rinv.

--- src/coqapprox/reals_tac.v
+++ src/coqapprox/reals_tac.v
@@ -5,18 +5,20 @@ Require Import Psatz.
 (** Ltac trick posted on coq-club by Frédéric Besson on 2015-04-05:
     https://sympa.inria.fr/sympa/arc/coq-club/2012-04/msg00025.html *)
 
-Ltac add_eq expr val := set (temp := expr) ; generalize (refl_equal temp) ; unfold temp at 1 ; generalize temp ; intro val ; clear temp.
+Ltac add_eq expr val :=
+  let temp := fresh in
+  set (temp := expr) ; generalize (refl_equal temp) ; unfold temp at 1 ; generalize temp ; intro val ; clear temp.
 
 Ltac elim_rinv :=
  match goal with
      | H : context [Rinv ?X] |- _ =>
          revert H ;
          let x := fresh in
-           add_eq X x ; generalize (Rinv_r x) ; generalize Rinv x ;
+           add_eq X x ; generalize (Rinv_r x) ; generalize (Rinv x) ;
          intros
      | |- context [Rinv ?X] =>
          let x := fresh in
-           add_eq X x ; generalize (Rinv_r x) ; generalize Rinv x ;
+           add_eq X x ; generalize (Rinv_r x) ; generalize (Rinv x) ;
          intros
  end.
 
