286352a0f80220ffbf840a34bc0e07b62a8c3df5
Erik Martin-Dorel <erik.martin-dorel@ens-lyon.org>
2015-12-26 14:58:48 +0100

    reals_tac.v: Rework elim_rinv.

--- src/coqapprox/reals_tac.v
+++ src/coqapprox/reals_tac.v
@@ -2,27 +2,38 @@ Require Import Reals.
 Require Import Rbase.
 Require Import Psatz.
 
-(** Ltac trick posted on coq-club by Frédéric Besson on 2015-04-05:
+(** Tactics inspired by Frédéric Besson's coq-club post on 2015-04-05:
     https://sympa.inria.fr/sympa/arc/coq-club/2012-04/msg00025.html *)
 
 Ltac add_eq expr val :=
   let temp := fresh in
-  set (temp := expr) ; generalize (refl_equal temp) ; unfold temp at 1 ; generalize temp ; intro val ; clear temp.
+  set (temp := expr) ;
+  generalize (refl_equal temp) ;
+  unfold temp at 1 ;
+  generalize temp ;
+  intro val ;
+  clear temp.
 
 Ltac elim_rinv :=
- match goal with
-     | H : context [Rinv ?X] |- _ =>
-         revert H ;
-         let x := fresh in
-           add_eq X x ; generalize (Rinv_r x) ; generalize (Rinv x) ;
-         intros
-     | |- context [Rinv ?X] =>
-         let x := fresh in
-           add_eq X x ; generalize (Rinv_r x) ; generalize (Rinv x) ;
-         intros
- end.
+  match goal with
+  | HX : context [Rinv ?X] |- _ =>
+    let r := fresh "r" in
+    let ri := fresh "ri" in
+    let Hri := fresh "Hri" in
+    let Hr := fresh "Hr" in
+    revert HX ;
+    add_eq X r ; generalize (Rinv_r r) ; generalize (Rinv r) ;
+    intros ri Hri Hr; intros
+  | |- context [Rinv ?X] =>
+    let r := fresh "r" in
+    let ri := fresh "ri" in
+    let Hri := fresh "Hri" in
+    let Hr := fresh "Hr" in
+    add_eq X r ; generalize (Rinv_r r) ; generalize (Rinv r) ;
+    intros ri Hri Hr; intros
+  end.
 
 Ltac simpl_R :=
- unfold Rdiv in * ; repeat elim_rinv.
+  unfold Rdiv in * ; repeat elim_rinv.
 
 (* Usage: 'simpl_R; psatzl R' or 'simpl_R; lra' *)
