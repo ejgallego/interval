2d6997d182598095dcab8828eb74d4519edfde5d
Thomas Sibut-Pinote <thomas.sibut-pinote@inria.fr>
2016-01-27 16:13:00 +0100

    Added new functions for computing integrals with Taylor models. We can now fall back on the naive method in case of failure. This allows to estimate RInt sqrt 0 1 for example.

--- src/Interval_integral_taylor.v
+++ src/Interval_integral_taylor.v
@@ -31,6 +31,7 @@ Module IntegralTacticTaylor (F : FloatOps with Definition even_radix := true).
 Module I := FloatInterval F.
 Module T := TranscendentalFloatFast F.
 Module Int := Integrability F.
+Module IntTac := IntegralTactic F.
 Module EF := ExtraFloats F.
 Import EF.
 (* Export EF. *)
@@ -46,15 +47,13 @@ Section DepthZeroPol.
 (* A fixed precision *)
 Variable prec : F.precision.
 
-Variables (f : R -> R) (* (iF : I.type -> I.type) *).
+Variables (f : R -> R) (iF : I.type -> I.type).
 
-(* This is a std monadic operation. Does it exist somewhere in the libs? *)
 Let g := toXreal_fun f.
 
-(* g is a restriction of f as an extendedR function. *)
 Let Hfgext := toXreal_fun_correct f.
 
-(* Hypothesis HiFIntExt : forall xi x, contains (I.convert xi) (Xreal x) -> contains (I.convert (iF xi)) (Xreal (f x)). *)
+Hypothesis HiFIntExt : forall xi x, contains (I.convert xi) (Xreal x) -> contains (I.convert (iF xi)) (Xreal (f x)).
 
 Variable Mf : TM.TMI.rpa.
 Variable X X0 : I.type.
@@ -85,35 +84,100 @@ Hypothesis Hcontx0 : contains iX0 (Xreal x0).
 Hypothesis Hconta : contains iX (Xreal (T.toR a)).
 Hypothesis Hcontb : contains iX (Xreal (T.toR b)).
 
+Section Functions.
+
 Definition taylor_integral :=
   TM.TMI.integralEnclosure prec X0 Mf ia ib.
+
+Definition deg := 10%nat. (* magic value, shouldn't be needed but for some reason it appears in the arguments of TaylorEvaluator in integrability *)
+
+(* Let iF1 := TM.TMI.ComputeBound prec Mf X0. *)
+
+Definition taylor_integral_subtle :=
+  match taylor_integral with
+    | Inan => IntTac.naive_integral prec iF a b
+    | x => x
+  end.
+
+(* now we take the intersection of a naive and intelligent way of computing the integral *)
+Definition taylor_integral_naive_intersection :=
+  let temp := IntTac.naive_integral prec iF a b in
+  match temp with
+    | Inan => Inan
+    | temp => match taylor_integral with
+                | Inan => temp
+                | x => I.meet x temp
+              end
+  end.
+
+(* Print TM.TMI.polIntegral. *)
+
+(* SearchAbout TM.TMI.rpa. *)
+(* Search _ TM.TMI.ComputeBound. *)
+(* Print IntTac.naive_integral. *)
+(* Print Int.TaylorEvaluator. *)
+
+End Functions.
+
+Section CorrectionLemmas.
+
+End CorrectionLemmas.
 
 Lemma taylor_integral_correct :
   contains
     (I.convert taylor_integral)
     (Xreal (RInt f (T.toR a) (T.toR b))).
 Proof.
-(*move: (@TM.TMI.integralEnclosure_correct prec ia ib Hcontx0 Hconta Hcontb).*)
+rewrite /taylor_integral.
+(* move: (@TM.TMI.integralEnclosure_correct prec ia ib (toXreal_fun f) Mf). *)
 (*apply: (TM.TMI.IntegralByPol Iprec _ _ _ _ _ _ _ validMf) => // .*)
 admit.
+Qed.
+
+Lemma taylor_integral_subtle_correct :
+  contains
+    (I.convert taylor_integral_subtle)
+    (Xreal (RInt f (T.toR a) (T.toR b))).
+Proof.
+rewrite /taylor_integral_subtle.
+case Htaylor_int : taylor_integral => [|l u].
+apply: IntTac.naive_integral_correct => // .
+rewrite -Htaylor_int.
+apply: taylor_integral_correct.
+Qed.
+
+Lemma taylor_integral_naive_intersection_correct :
+  contains
+    (I.convert taylor_integral_naive_intersection)
+    (Xreal (RInt f (T.toR a) (T.toR b))).
+Proof.
+rewrite /taylor_integral_naive_intersection.
+set tmp := (IntTac.naive_integral _ _ _ _).
+case Ht : tmp => [| l u] => // .
+set int := Xreal _.
+case Hti : (taylor_integral) => [| l1 u1]; rewrite -Ht.
+- by apply: IntTac.naive_integral_correct.
+- rewrite -Hti; apply: (I.meet_correct taylor_integral tmp).
+  apply: taylor_integral_correct.
+  by apply: IntTac.naive_integral_correct.
 Qed.
 
 End DepthZeroPol.
 
 Section Depth_Sk_Pol.
 
-Variable getRpa : I.type -> TM.TMI.rpa. (* general parameter for any depth, including zero -> GO UP*)
+(* Variable getRpa : I.type -> TM.TMI.rpa. (* general parameter for any depth, including zero -> GO UP*) *)
 
 End Depth_Sk_Pol.
 
 End IntegralTacticTaylor.
 
 
-Require Import Interval_bigint_carrier.
-Require Import Interval_specific_ops.
-Module SFBI2 := SpecificFloat BigIntRadix2.
-Module IT := IntegralTacticTaylor SFBI2.
-Require Import BigZ.
+(* Require Import Interval_bigint_carrier. *)
+(* Require Import Interval_specific_ops. *)
+(* Module SFBI2 := SpecificFloat BigIntRadix2. *)
+(* Module IT := IntegralTacticTaylor SFBI2. *)
+(* Require Import BigZ. *)
 
 (* Eval vm_compute in ( *)
 (*   let prec := 20%bigZ in *)
