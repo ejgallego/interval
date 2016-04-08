8787308015b5e85472104e6215bac68f1e279ba2
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2016-04-08 11:25:11 +0200

    New release.

--- testsuite/example-20071016.v
+++ testsuite/example-20071016.v
@@ -1,25 +1,27 @@
 Require Import Reals.
-Require Import Interval_tactic.
+Require Import Interval.Interval_tactic.
+
+Open Scope R_scope.
 
 Goal
-  forall x, (-1 <= x <= 1)%R ->
-  (sqrt (1 - x) <= 3/2)%R.
+  forall x, -1 <= x <= 1 ->
+  sqrt (1 - x) <= 3/2.
 Proof.
   intros.
   interval.
 Qed.
 
 Goal
-  forall x, (-1 <= x <= 1)%R ->
-  (sqrt (1 - x) <= 141422/100000)%R.
+  forall x, -1 <= x <= 1 ->
+  sqrt (1 - x) <= 141422/100000.
 Proof.
   intros.
   interval.
 Qed.
 
 Goal
-  forall x, (-1 <= x <= 1)%R ->
-  (sqrt (1 - x) <= 141422/100000)%R.
+  forall x, -1 <= x <= 1 ->
+  sqrt (1 - x) <= 141422/100000.
 Proof.
   intros.
   interval_intro (sqrt (1 - x)) upper as H'.
@@ -28,29 +30,38 @@ Proof.
 Qed.
 
 Goal
-  forall x, (3/2 <= x <= 2)%R ->
-  forall y, (1 <= y <= 33/32)%R ->
-  (Rabs (sqrt(1 + x/sqrt(x+y)) - 144/1000*x - 118/100) <= 71/32768)%R.
+  forall x, 3/2 <= x <= 2 ->
+  forall y, 1 <= y <= 33/32 ->
+  Rabs (sqrt(1 + x/sqrt(x+y)) - 144/1000*x - 118/100) <= 71/32768.
 Proof.
   intros.
   interval with (i_prec 19, i_bisect x).
 Qed.
 
 Goal
-  forall x, (1/2 <= x <= 2)%R ->
-  (Rabs (sqrt x - (((((122 / 7397 * x + (-1733) / 13547) * x
+  forall x, 1/2 <= x <= 2 ->
+  Rabs (sqrt x - (((((122 / 7397 * x + (-1733) / 13547) * x
                    + 529 / 1274) * x + (-767) / 999) * x
                    + 407 / 334) * x + 227 / 925))
-    <= 5/65536)%R.
+    <= 5/65536.
 Proof.
   intros.
   interval with (i_bisect_taylor x 3).
 Qed.
 
 Goal
-  forall x, (-1 <= x)%R ->
-  (x < 1 + powerRZ x 3)%R.
+  forall x, -1 <= x ->
+  x < 1 + powerRZ x 3.
 Proof.
   intros.
   interval with (i_bisect_diff x).
+Qed.
+
+Require Import Coquelicot.Coquelicot.
+
+Goal
+  Rabs (RInt (fun x => atan (sqrt (x*x + 2)) / (sqrt (x*x + 2) * (x*x + 1))) 0 1
+        - 5/96*PI*PI) <= 1/1000.
+Proof.
+  interval with (i_integral_prec 9, i_integral_depth 1, i_integral_deg 5).
 Qed.
