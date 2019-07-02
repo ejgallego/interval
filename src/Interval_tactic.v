eb28a5ceaa9b232b70e62f07f5f2de36c6ee8a97
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2019-07-02 11:18:37 +0200

    Restore Interval_tactic for backward compatibility.

---
+++ src/Interval_tactic.v
@@ -0,0 +1,20 @@
+(**
+This file is part of the Coq.Interval library for proving bounds of
+real-valued expressions in Coq: http://coq-interval.gforge.inria.fr/
+
+Copyright (C) 2019, Inria
+
+This library is governed by the CeCILL-C license under French law and
+abiding by the rules of distribution of free software. You can use,
+modify and/or redistribute the library under the terms of the CeCILL-C
+license as circulated by CEA, CNRS and Inria at the following URL:
+http://www.cecill.info/
+
+As a counterpart to the access to the source code and rights to copy,
+modify and redistribute granted by the license, users are provided
+only with a limited warranty and the library's author, the holder of
+the economic rights, and the successive licensors have only limited
+liability. See the COPYING file for more details.
+*)
+
+Require Export Tactic.
