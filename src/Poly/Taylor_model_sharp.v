bac89ae47b30433bf75d44e0a49b263e7a5058e6
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2019-07-05 17:11:58 +0200

    Remove file Coquelicot_compl.v.

--- src/Poly/Taylor_model_sharp.v
+++ src/Poly/Taylor_model_sharp.v
@@ -37,7 +37,6 @@ Require Import Datatypes.
 Require Import Taylor_poly.
 Require Import Basic_rec.
 Require Import Bound.
-Require Import Coquelicot_compl.
 Require Import Polynom.
 
 (********************************************************************)
@@ -1392,6 +1391,47 @@ constructor.
 - move=> *; apply/ex_derive_n_is_derive_n/is_derive_n_cos.
 Qed.
 
+Lemma is_derive_n_atan n (x : R) :
+  let q n := iteri n
+    (fun i c => PolR.div_mixed_r tt (PolR.sub tt (PolR.add tt c^`() (PolR.lift 2 c^`()))
+      (PolR.mul_mixed tt (INR (i.+1).*2) (PolR.lift 1 c))) (INR i.+2))
+    PolR.one in
+  is_derive_n atan n x
+  (if n is n.+1 then PolR.horner tt (q n) x / (1 + x²) ^ (n.+1) * INR (fact n.+1)
+   else atan x)%R.
+Proof.
+move=> q; move: n x.
+help_is_derive_n_whole n x.
+- rewrite /Rdiv !Rsimpl.
+  have := is_derive_atan x; exact: is_derive_ext.
+- set Q := (1 + x * x)%R.
+  have HQ : Q <> 0%R by exact: Rsqr_plus1_neq0.
+  have HQ' : (Q ^ n <> 0)%R by exact: pow_nonzero.
+  have HQ'' : (Q * Q ^ n <> 0)%R.
+    rewrite Rmult_comm -(Rmult_0_l (1 + x * x)).
+    by apply:Rmult_neq_compat_r; try apply: pow_nonzero; exact: Rsqr_plus1_neq0.
+  auto_derive; first split.
+  + exact: PolR.ex_derive_horner.
+  + by split.
+  + rewrite Rmult_1_l PolR.Derive_horner [q n.+1]iteriS -/(q n).
+    rewrite PolR.horner_div_mixed_r PolR.horner_sub PolR.horner_add.
+    rewrite PolR.horner_mul_mixed !PolR.horner_lift.
+    rewrite -{1}[in RHS](Rmult_1_r (q n)^`().[x]) -Rmult_plus_distr_l.
+    have->: (x ^ 2 = x²)%R by simpl; rewrite Rmult_1_r.
+    rewrite pow_1 /Rdiv.
+    have H1 : (if n is _.+1 then (INR n + 1)%R else 1%R) = INR n.+1 by [].
+    rewrite H1.
+    rewrite !Rinv_mult_distr // -/pow -/Q.
+    have->: INR (fact n + (n * fact n))%coq_nat = INR (fact n.+1) by [].
+    rewrite [in RHS]fact_simpl mult_INR.
+    set A := (((q n)^`()).[x] * Q - INR (n.+1).*2 * ((q n).[x] * x))%R.
+    have->: (A * / INR n.+2 * (/ Q * (/ Q * / Q ^ n)) * (INR n.+2 * INR (fact n.+1)) =
+      A * (/ INR n.+2 * INR n.+2) * (/ Q * (/ Q * / Q ^ n)) * INR (fact n.+1))%R by ring.
+    rewrite Rinv_l; last exact: not_0_INR.
+    rewrite /A !Rsimpl. rewrite -mul2n [INR (2 * _)%N]mult_INR; simpl (INR 2).
+    field; by split.
+Qed.
+
 Lemma size_TM_atan (X0 X : I.type) (n : nat) :
   Pol.size (approx (TM_atan X0 X n)) = n.+1.
 Proof. by rewrite Pol.size_grec1. Qed.
@@ -1485,6 +1525,42 @@ Definition TM_tan (X0 X : I.type) (n : nat) : rpa :=
   if apart0 ic
   then RPA P (Ztech (T_tan prec) P (I.tan prec) X0 X n)
   else RPA P I.nai.
+
+Lemma is_derive_n_tan n x :
+  cos x <> 0%R ->
+  let q n := iteri n
+    (fun i c => PolR.div_mixed_r tt (PolR.add tt c^`() (PolR.lift 2 c^`())) (INR i.+1))
+    (PolR.lift 1 PolR.one) in
+  is_derive_n tan n x ((q n).[tan x] * INR (fact n)).
+Proof.
+move=> Hx q; move: n x Hx.
+help_is_derive_n n x.
+- by move=> x Hx; rewrite /= !Rsimpl.
+- move=> Hx; auto_derive.
+  + by eapply ex_derive_is_derive; apply: is_derive_tan.
+  + rewrite !Rsimpl; rewrite (is_derive_unique _ _ _ (is_derive_tan _ Hx)).
+    ring.
+- exact: (open_comp cos (fun y => y <> 0%R)
+                    (fun y _ => continuous_cos y) (open_neq 0%R)).
+- move=> x Hx.
+  move En1 : n.+1 => n1.
+  (* Remember n.+1 as n1 to have a more tidy context after auto_derive *)
+  auto_derive; repeat split.
+  + apply: PolR.ex_derive_horner.
+  + by eapply ex_derive_is_derive; apply: is_derive_tan.
+  + rewrite Rmult_1_l.
+    rewrite (is_derive_unique _ _ _ (is_derive_tan _ Hx)).
+    rewrite PolR.Derive_horner [q n1.+1]iteriS -/(q n1).
+    rewrite PolR.horner_div_mixed_r PolR.horner_add.
+    rewrite !PolR.horner_lift.
+    rewrite fact_simpl mult_INR.
+    set A := (((q n1)^`()).[tan x] + ((q n1)^`()).[tan x] * tan x ^ 2)%R.
+    rewrite /Rdiv.
+    have->: (A * / INR n1.+1 * (INR n1.+1 * INR (fact n1)) =
+             A * INR (fact n1) * (/ INR n1.+1 * INR n1.+1))%R by ring.
+    rewrite Rinv_l; last exact: not_0_INR.
+    rewrite /A; ring.
+Qed.
 
 Lemma size_TM_tan (X0 X : I.type) (n : nat) :
   Pol.size (approx (TM_tan X0 X n)) = n.+1.
@@ -1591,7 +1667,7 @@ constructor.
     apply: apart0_correct E0.
     exact: J.cos_correct.
   eapply (@ex_derive_n_ext_loc tan); last first.
-    exact: ex_derive_n_is_derive_n (is_derive_n_tan m x E0).
+    exact: ex_derive_n_is_derive_n (is_derive_n_tan m E0).
   eapply locally_open with
     (1 := open_comp cos (fun y => y <> 0%R)
       (fun y _ => continuous_cos y)
