562f99ffe0484f84b27acda7f69d3e7492142484
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2019-12-09 06:27:16 +0100

    Adapt to Coq master.

--- src/Float/Generic_proof.v
+++ src/Float/Generic_proof.v
@@ -1002,7 +1002,7 @@ rewrite Zdiv_eucl_unique.
 set (q := (_ / _)%Z).
 set (r := (_ mod _)%Z).
 assert (Pq : (0 < q)%Z).
-  apply  Z.div_str_pos; split; try lia.
+  apply  Z.div_str_pos; split; try easy; try lia.
   generalize (Zdigits_correct beta (Zpos p)); intros [U1 U2].
   apply Z.le_trans with (2 := U1).
   rewrite Pos2Z.inj_pow, radix_to_pos.
