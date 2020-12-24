16ba902041175b0b1d9aca2a91b03addafb505f0
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2020-12-24 10:45:54 +0100

    Restrict reification of bpow to literal arguments.

--- testsuite/bug-20201223.v
+++ testsuite/bug-20201223.v
@@ -1,7 +1,16 @@
 From Coq Require Import Reals.
+From Flocq Require Import Core.
 From Interval Require Import Tactic.
 
 Goal forall x, (IZR (1 + 1) <= IZR x -> 1 <= sqrt (IZR x))%R.
+Proof.
+intros.
+interval.
+Qed.
+
+Goal forall prec,
+  (0 <= bpow radix2 (1 - prec) <= 1 / 32 ->
+  12 / 25 <= (2 - bpow radix2 (1 - prec)) / (2 * (2 + bpow radix2 (1 - prec))))%R.
 Proof.
 intros.
 interval.
