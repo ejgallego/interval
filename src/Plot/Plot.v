77c346a68656e4d5585885fd11c7f227fee8377c
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2021-01-28 18:55:02 +0100

    Use 1-pixel-wide rectangles instead of arbitrary rectangles.
    This commit also uses the computed polynomial as long as it gives tight
    enclosures for every pixel. An enclosure P([u,v]) of f is deemed tight if
    it is almost the same as an underapproximation of [f(u),f(v)]. If not, a
    new polynomial is computed on a smaller range of pixels.
    
    If a polynomial is successful for all the pixels of the original range, a
    larger range is chosen next. This makes it possible to dynamically adjust
    the accuracy of the computations.

--- src/Plot/Plot.v
+++ src/Plot/Plot.v
@@ -9,38 +9,52 @@ Import IT.Private.IT1.
 
 Module F := SFBI2.
 
-Definition plot (f : R -> R) (u v : R) (l : list (I.type * I.type)) :=
-  forall x, (u <= x <= v)%R ->
-  Exists (fun p : I.type * I.type => let (xi, yi) := p in
-    contains (I.convert xi) (Xreal x) /\ contains (I.convert yi) (Xreal (f x))) l.
+Definition plot (f : R -> R) (u d : R) (l : list I.type) :=
+  forall i x, (u + d * INR i <= x <= u + d * INR (S i))%R ->
+  contains (I.convert (nth i l I.nai)) (Xreal (f x)).
 
 Lemma plot_ext :
-  forall f g u v l,
+  forall f g u d l,
   (forall x, f x = g x) ->
-  plot f u v l -> plot g u v l.
+  plot f u d l -> plot g u d l.
 Proof.
-intros f g u v l H K x Hx.
-specialize (K x Hx).
-induction K as [[xi yi] K| [xi yi] l _ IHK].
-apply Exists_cons_hd.
+intros f g u d l H K i x Hx.
+specialize (K i x Hx).
 now rewrite <- H.
-now apply Exists_cons_tl.
 Qed.
 
-Fixpoint eval_plot_aux (fi : I.type -> I.type) check n xi acc :=
-  let yi := fi xi in
-  match n with
-  | S n =>
-    match check yi with
-    | true => (xi, yi) :: acc
-    | false =>
-      let m := I.bisect xi in
-      eval_plot_aux fi check n (fst m) (eval_plot_aux fi check n (snd m) acc)
-    end
-  | O => (xi, yi) :: acc
+Fixpoint eval_plot_aux prec (gi : I.type -> I.type -> I.type) (check : I.type -> bool) (ui di zi2 : I.type) (fi : I.type -> I.type) (i : nat) (mz xz rz : Z) (acc : list I.type) : list I.type :=
+  match i with
+  | O => acc
+  | S i =>
+    let xz' := Z.pred xz in
+    let xi1 := I.add prec ui (I.mul prec di (I.fromZ prec xz')) in
+    let xi2 := I.add prec ui (I.mul prec di (I.fromZ prec xz)) in
+    let xi := I.join xi1 xi2 in
+    let zi1 := fi xi1 in
+    let yi := fi xi in
+    let c := andb
+      (orb (check (I.meet yi (I.lower_extent zi1))) (check (I.meet yi (I.upper_extent zi1))))
+      (orb (check (I.meet yi (I.lower_extent zi2))) (check (I.meet yi (I.upper_extent zi2)))) in
+    let yizi :=
+      if c then (yi, zi1)
+      else let fi := gi xi in (fi xi, fi xi1) in
+    let mz' :=
+      if Z.eqb mz xz' then (mz - Z.div2 (3 * rz))%Z
+      else if c then mz
+      else (xz' - 1 - Z.div2 rz)%Z in
+    let mz' :=
+      if Z.ltb mz' 0%Z then 0%Z else mz' in
+    let firz :=
+      if Z.eqb mz mz' then (fi, rz)
+      else
+        let xi0 := I.add prec ui (I.mul prec di (I.fromZ prec mz')) in
+        (gi (I.join xi0 xi1), (xz' - mz'))%Z in
+    let acc := fst yizi :: acc in
+    eval_plot_aux prec gi check ui di (snd yizi) (fst firz) i mz' xz' (snd firz) acc
   end.
 
-Definition eval_plot prec deg limit check hyps pf pu pv cf cu cv :=
+Definition eval_plot prec deg check hyps pf pu pv cf cu cv nb :=
   let hyps := R.merge_hyps prec hyps in
   let ui :=
     let bounds := hyps ++ map (T.eval_bnd prec) cu in
@@ -48,22 +62,26 @@ Definition eval_plot prec deg limit check hyps pf pu pv cf cu cv :=
   let vi :=
     let bounds := hyps ++ map (T.eval_bnd prec) cv in
     nth 0 (A.BndValuator.eval prec pv bounds) I.nai in
-  let fi :=
+  let gi :=
     let bounds := hyps ++ map (T.eval_bnd prec) cf in
     let bounds := A.TaylorValuator.TM.var :: map A.TaylorValuator.TM.const bounds in
-    fun xi =>
-      A.TaylorValuator.TM.eval (prec, deg)
-        (nth 0 (A.TaylorValuator.eval prec deg xi pf bounds)
-        A.TaylorValuator.TM.dummy) xi xi in
-  eval_plot_aux fi check limit (I.join ui vi) nil.
+    fun yi =>
+      let fi := nth 0 (A.TaylorValuator.eval prec deg yi pf bounds) A.TaylorValuator.TM.dummy in
+      fun xi => A.TaylorValuator.TM.eval (prec, deg) fi yi xi in
+  let di := I.div prec (I.sub prec vi ui) (I.fromZ prec nb) in
+  let ui' := I.add prec ui (I.mul prec di (I.fromZ prec 0)) in
+  let vi' := I.add prec ui (I.mul prec di (I.fromZ prec nb)) in
+  eval_plot_aux prec gi check ui di I.whole (gi (I.join ui' vi')) (Z.to_nat nb) 0%Z nb nb nil.
 
 Lemma eval_plot_correct :
-  forall prec deg limit check vars hyps pf pu pv cf cu cv l,
-  eval_plot prec deg limit check hyps pf pu pv cf cu cv = l ->
+  forall prec deg check vars hyps pf pu pv cf cu cv nb l,
+  eval_plot prec deg check hyps pf pu pv cf cu cv (Zpos nb) = l ->
+  let u := eval_real' pu vars cu in
+  let v := eval_real' pv vars cv in
   eval_hyps hyps vars (
-    plot (fun t => eval_real' pf (t :: vars) cf) (eval_real' pu vars cu) (eval_real' pv vars cv) l).
+    plot (fun t => eval_real' pf (t :: vars) cf) u ((v - u) / IZR (Zpos nb)) l).
 Proof.
-intros prec deg limit check vars hyps pf pu pv cf cu cv l H.
+intros prec deg check vars hyps pf pu pv cf cu cv nb l H.
 apply (R.eval_hyps_bnd_correct prec).
 intros H'.
 revert H.
@@ -82,50 +100,78 @@ clear -H'.
 intros u v ui vi Hu Hv.
 intros <-.
 set (bounds := A.TaylorValuator.TM.var :: _).
-set (fi xi := A.TaylorValuator.TM.eval (prec, deg) (nth 0 (A.TaylorValuator.eval prec deg xi pf bounds) A.TaylorValuator.TM.dummy) xi xi).
-unfold plot.
-intros x Hx.
+set (di := I.div prec (I.sub prec vi ui) (I.fromZ prec (Zpos nb))).
+set (ui' := I.add prec ui (I.mul prec di (I.fromZ prec 0))).
+set (vi' := I.add prec ui (I.mul prec di (I.fromZ prec (Zpos nb)))).
+set (gi := fun yi =>
+      let fi := nth 0 (A.TaylorValuator.eval prec deg yi pf bounds) A.TaylorValuator.TM.dummy in
+      fun xi => A.TaylorValuator.TM.eval (prec, deg) fi yi xi).
 set (f x := nth 0 (eval_real pf ((x :: vars) ++ map (fun c : expr => eval c nil) cf)) 0).
-set (P := fun p : I.type * I.type => let (xi, yi) := p in
-    contains (I.convert xi) (Xreal x) /\ contains (I.convert yi) (Xreal (f x))).
-change (Exists P (eval_plot_aux fi check limit (I.join ui vi) nil)).
-generalize (A.J.join_correct _ _ _ _ _ Hu Hv Hx).
-generalize (I.join ui vi).
-clear -H' P.
-generalize (@nil (I.type * I.type)).
-clear -H'.
-induction limit as [|n IH].
-- intros acc xi Hx.
-  apply Exists_cons_hd.
-  apply (conj Hx).
-  simpl.
+fold (gi (I.join ui' vi')).
+set (d := ((v - u) / IZR (Zpos nb))%R).
+assert (Hd : contains (I.convert di) (Xreal d)).
+{ apply J.div_correct.
+  now apply J.sub_correct.
+  apply I.fromZ_correct. }
+assert (Hg: forall ti xi x, contains (I.convert xi) (Xreal x) -> contains (I.convert (gi ti xi)) (Xreal (f x))).
+{ intros ti xi x Hx.
   apply A.TaylorValuator.eval_correct with (2 := Hx).
-  now apply app_merge_hyps_eval_bnd.
-- intros acc xi Hx.
-  simpl.
-  destruct check.
-  + apply Exists_cons_hd.
-    apply (conj Hx).
-    simpl.
-    apply A.TaylorValuator.eval_correct with (2 := Hx).
-    now apply app_merge_hyps_eval_bnd.
-  + destruct (I.bisect_correct xi _ Hx) as [H|H].
-    now apply IH.
-    generalize (IH acc _ H).
-    clear.
-    generalize (eval_plot_aux fi check n (snd (I.bisect xi)) acc).
-    generalize (fst (I.bisect xi)).
-    clear.
-    induction n.
-    * intros xi acc.
-      apply Exists_cons_tl.
-    * intros xi acc.
-      simpl.
-      destruct check.
-      apply Exists_cons_tl.
-      intros H.
-      apply IHn.
-      now apply IHn.
+  now apply app_merge_hyps_eval_bnd. }
+rewrite <- (Z2Nat.id (Zpos nb)) at 2 by easy.
+set (i := Z.to_nat (Zpos nb)).
+cut (plot f (u + d * INR i) d nil).
+2: now intros [|j] x _.
+generalize (@nil I.type).
+generalize I.whole (I.join ui' vi') 0%Z (Z.pos nb).
+clearbody f gi di d i.
+clear -Hu Hd Hg.
+induction i as [|n IH].
+{ simpl. intros _ _ _ l. now rewrite Rmult_0_r, Rplus_0_r. }
+intros zi2 gxi mz rz acc Hacc.
+cbn beta iota zeta delta [eval_plot_aux].
+rewrite <- Nat2Z.inj_pred by apply lt_O_Sn.
+simpl (Nat.pred (S n)).
+set (xi1 := I.add prec ui (I.mul prec di (I.fromZ prec (Z.of_nat n)))).
+set (xi2 := I.add prec ui (I.mul prec di (I.fromZ prec (Z.of_nat (S n))))).
+set (xi := I.join xi1 xi2).
+set (zi1 := gi gxi xi1).
+set (yi := gi gxi xi).
+set (c := andb
+  (orb (check (I.meet yi (I.lower_extent zi1))) (check (I.meet yi (I.upper_extent zi1))))
+  (orb (check (I.meet yi (I.lower_extent zi2))) (check (I.meet yi (I.upper_extent zi2))))).
+clearbody c.
+set (yizi := if c then (yi, zi1) else (gi xi xi, gi xi xi1)).
+set (mz' :=
+  if Z.eqb mz (Z.of_nat n) then (mz - Z.div2 (3 * rz))%Z
+  else if c then mz
+  else (Z.of_nat n - 1 - Z.div2 rz)%Z).
+set (mz'' := if Z.ltb mz' 0%Z then 0%Z else mz').
+clearbody mz' mz''.
+set (gxi' := I.join (I.add prec ui (I.mul prec di (I.fromZ prec mz''))) xi1).
+clearbody gxi'.
+cut (plot f (u + d * INR n) d (fst yizi :: acc)).
+{ destruct Z.eqb ; apply IH. }
+clear -Hu Hd Hg Hacc.
+intros [|i] x Hx.
+2: {
+  apply Hacc.
+  revert Hx. clear.
+  rewrite !S_INR.
+  replace (u + d * INR n + d * (INR i + 1)) with (u + d * (INR n + 1) + d * INR i) by ring.
+  replace (u + d * INR n + d * (INR i + 1 + 1)) with (u + d * (INR n + 1) + d * (INR i + 1)) by ring.
+  easy. }
+assert (Hxi: contains (I.convert xi) (Xreal x)).
+{ apply J.join_correct with (u := u + d * (IZR (Z.of_nat n))) (v := u + d * (IZR (Z.of_nat (S n)))).
+  apply J.add_correct with (1 := Hu).
+  apply J.mul_correct with (1 := Hd).
+  apply I.fromZ_correct.
+  apply J.add_correct with (1 := Hu).
+  apply J.mul_correct with (1 := Hd).
+  apply I.fromZ_correct.
+  rewrite Nat2Z.inj_succ, succ_IZR, <- INR_IZR_INZ.
+  revert Hx. clear.
+  now rewrite Rmult_0_r, Rplus_0_r, Rmult_plus_distr_l, Rplus_assoc. }
+now destruct c ; simpl ; apply Hg.
 Qed.
 
 Import Float Specific_ops BigZ.
@@ -133,10 +179,13 @@ Import Float Specific_ops BigZ.
 Open Scope bigZ_scope.
 
 Goal True.
-evar (p : list (I.type * I.type)).
-assert (plot (fun x => exp (sin x)) 0 1 p).
+Proof.
+evar (p : list I.type).
+assert (plot (fun x => x^2 * sin (x^2))%R (-4) ((4 - (-4)) / IZR 1000) p).
 match goal with
-| |- plot ?f ?u ?v ?l =>
+| |- plot ?f ?u ?d ?l =>
+match d with
+| ((?v - u) / IZR (Zpos ?d))%R =>
 let fapp := eval cbv beta in (f reify_var) in
 let vars := constr:((reify_var :: nil)%list) in
 let vars := get_vars fapp vars in
@@ -154,9 +203,10 @@ eapply plot_ext ; [
 reify_partial u vars ; intros <- ;
 reify_partial v vars ; intros <- ;
 find_hyps vars
+end
 end.
 apply eval_plot_correct with
- (prec := F.PtoP 30) (deg := 10%nat) (limit := 10%nat)
+ (prec := F.PtoP 30) (deg := 10%nat)
  (check := let prec := F.PtoP 30 in let thr := F.scale (F.fromZ 1) (F.ZtoS (-5)) in
   fun yi => F'.le' (F.sub_UP prec (I.upper yi) (I.lower yi)) thr).
 vm_compute ; apply eq_refl.
